c0aa0667c554882b8ec06e124d978f5015ac2d5f
Muktha P <mukthamohanan18@gmail.com>
2025-09-21 21:43:52 +0530

    added python file

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def calc(op,a,b):
+    if op=='+':
+        print(a+b)
+    elif op=='-':
+        print(a-b)    
+    elif op=='*':
+        print(a*b)    
+    elif op=='/':
+        print(a/b)
+    else:
+        print("Not a valid operation")
+
+
+calc('/',3,4)            
